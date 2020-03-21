e49cc5161f47eb620f15004fd332b87911d90ca7
tonymig <60812114+tonymig@users.noreply.github.com>
2020-03-21 21:21:55 +0300

    init: completed typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array[0] == undefined) {
+      return 0;
+  }
+  else {
+      let arrSort = array.sort(function(a, b){return a - b});
+      return arrSort[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array[0] == undefined) {
+      return 0;
+  }
+  else {
+      let arrSort = array.sort(function(a, b){return a - b});
+      return arrSort[array.length - 1];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array[0] == undefined) {
+      return 0;
+  }
+  else {
+      let arrAvg = 0;
+      for (let i = 0; i < array.length; i++) {
+          arrAvg += array[i];
+      }
+      return arrAvg = arrAvg/array.length;
+  }
 }
